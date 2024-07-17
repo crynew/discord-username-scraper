fe566bf1e0c1d41e4936ee9a541ad2e572edcf3d
Brunno <145746324+crynew@users.noreply.github.com>
2024-07-17 15:36:56 -0300

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { Client } from 'discord.js-selfbot-v13';
+
+const client = new Client({});
+
+const usernameSniper = "" 
+
+client.on('ready', () => {
+  console.log(`[Client] Conectado: ${client.user?.tag}!`);
+});
+
+client.on('userUpdate', async (oldUser, newUser) => {
+  if (oldUser.username !== newUser.username) {
+    if (newUser.username === usernameSniper) {
+      console.log(`O username desejado '${usernameSniper}' foi liberado!`);
+    } else {
+      console.log(`O usuário ${oldUser.username} mudou o nome de usuário para ${newUser.username}`);
+    }
+  }
+});
+
+client.login('TOKEN'); 
